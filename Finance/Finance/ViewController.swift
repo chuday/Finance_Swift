6882105b766e83ad77951ea4bd377ce95dbbc3c9
chuday <mikhail_chudaew505@mail.ru>
2021-11-10 07:08:30 +0500

    add realm

--- Finance/Finance/ViewController.swift
+++ Finance/Finance/ViewController.swift
@@ -6,8 +6,13 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
+    
+    let realm = try! Realm()
+    
+    var spendingArray: Results<Spending>!
     
     var stillTyping = false
     
@@ -22,19 +27,23 @@ class ViewController: UIViewController {
     }
     
     var categoryName = ""
-    var displayValue = ""
+    var displayValue: Int = 1
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        spendingArray = realm.objects(Spending.self)
     }
     
     @IBAction func numberPressed(_ sender: UIButton) {
         let number = sender.titleLabel?.text
         
-        if stillTyping {
+        if stillTyping == true {
+            if displayLabel.text == "0" {
+                displayLabel.text = ""
+            }
             if displayLabel.text!.count < 15 {
                 displayLabel.text = displayLabel.text! + number!
             }
@@ -52,28 +61,63 @@ class ViewController: UIViewController {
     
     @IBAction func categoryPressed(_ sender: UIButton) {
         categoryName = (sender.titleLabel?.text)!
-        displayValue = displayLabel.text!
+        displayValue = Int(displayLabel.text!)!
         
         displayLabel.text = "0"
         stillTyping = false
         
-        print(categoryName)
-        print(displayValue)
-        
+        let value = Spending(value: ["\(categoryName)", displayValue])
+        try! realm.write {
+            realm.add(value)
+            
+            print("File realm: \(realm.configuration.fileURL)")
+            
+        }
+        tableView.reloadData()
     }
-    
 }
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        
+        return spendingArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
+        
+        let spending = spendingArray[indexPath.row]
+        
+        cell.recordCategory.text = spending.category
+        cell.recordCost.text = "\(spending.cost)"
+        
+        switch spending.category {
+            case "еда": cell.recordImage.image = UIImage(systemName: "circle.grid.cross")
+            case "одежда": cell.recordImage.image = UIImage(systemName: "tshirt")
+            case "связь": cell.recordImage.image = UIImage(systemName: "phone.connection")
+            case "досуг": cell.recordImage.image = UIImage(systemName: "book")
+            case "авто": cell.recordImage.image = UIImage(systemName: "car")
+            case "красота": cell.recordImage.image = UIImage(systemName: "heart.square")
+            
+            default: cell.recordImage.image = UIImage(named: "pencil.slash")
+            
+        }
         
         return cell
     }
     
-    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        
+        let editingRow = spendingArray[indexPath.row]
+        
+        let deleteAction = UITableViewRowAction(style: .destructive, title: "удалить") { (_, _) in
+            try! self.realm.write{
+                self.realm.delete(editingRow)
+                tableView.reloadData()
+            }
+        }
+        
+        return [deleteAction]
+        
+    }
 }
