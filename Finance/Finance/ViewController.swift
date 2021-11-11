712bdc434e4ef30d766b993f3415647bd0592fe0
chuday <mikhail_chudaew505@mail.ru>
2021-11-11 22:40:52 +0500

    add alert controller and revers data tableView

--- Finance/Finance/ViewController.swift
+++ Finance/Finance/ViewController.swift
@@ -16,6 +16,10 @@ class ViewController: UIViewController {
     
     var stillTyping = false
     
+    @IBOutlet weak var limitLabel: UILabel!
+    @IBOutlet weak var howMoneyCanSpend: UILabel!
+    @IBOutlet weak var spendByCheck: UILabel!
+    
     @IBOutlet weak var displayLabel: UILabel!
     
     @IBOutlet var numberFromKeyboards: [UIButton]! {
@@ -75,6 +79,62 @@ class ViewController: UIViewController {
         }
         tableView.reloadData()
     }
+    
+    @IBAction func limitPressed(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Установить лимит",
+                                                message: "Введите сумму и количество дней",
+                                                preferredStyle: .alert)
+        
+        let alertInstall = UIAlertAction(title: "Установить", style: .default) { action in
+            
+            let textFieldSum = alertController.textFields?[0].text
+            self.limitLabel.text = textFieldSum
+            
+            let textFieldDate = alertController.textFields?[1].text
+            
+            guard textFieldDate != "" else { return }
+            
+            if let day = textFieldDate {
+                let dateNow = Date()
+                let lastDay: Date = dateNow.addingTimeInterval(60 * 60 * 24 * Double(day)!)
+                
+                let limit = self.realm.objects(Limit.self)
+                
+                if limit.isEmpty == true {
+                    let value = Limit(value: [self.limitLabel.text, dateNow, lastDay])
+                    try! self.realm.write {
+                        self.realm.add(value)
+                        
+                        print("File realm: \(self.realm.configuration.fileURL)")
+                    }
+                    
+                } else {
+                    try! self.realm.write{
+                        limit[0].limitSum = self.self.limitLabel.text!
+                        limit[0].limitDate = dateNow as NSDate
+                        limit[0].limitLastDay = lastDay as NSDate
+                    }
+                }
+            }
+        }
+        
+        alertController.addTextField { (money) in
+            money.placeholder = "Сумма"
+            money.keyboardType = .asciiCapableNumberPad
+        }
+        
+        alertController.addTextField { (day) in
+            day.placeholder = "Количество дней"
+            day.keyboardType = .asciiCapableNumberPad
+        }
+        
+        let alertCancel = UIAlertAction(title: "Отмена", style: .default) { _ in }
+        
+        alertController.addAction(alertInstall)
+        alertController.addAction(alertCancel)
+        
+        present(alertController, animated: true, completion: nil)
+    }
 }
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
@@ -86,7 +146,7 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
         
-        let spending = spendingArray[indexPath.row]
+        let spending = spendingArray.reversed()[indexPath.row]
         
         cell.recordCategory.text = spending.category
         cell.recordCost.text = "\(spending.cost)"
@@ -108,7 +168,7 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
         
-        let editingRow = spendingArray[indexPath.row]
+        let editingRow = spendingArray.reversed()[indexPath.row]
         
         let deleteAction = UITableViewRowAction(style: .destructive, title: "удалить") { (_, _) in
             try! self.realm.write{
